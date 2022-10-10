a3ae6bf42ea1148f9b72dd8f88cfb91067632023
theocita <theocitaallen@gmail.com>
2022-10-09 20:20:54 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers,string){
+     const results = drivers.filter(driver => driver == string);
+     return results;
+};
+
+function fuzzyMatch(drivers, string){
+    const match = drivers.filter(driver => driver.charAt(0) == string)
+    return match
+}
+
+function matchName(drivers, string){
+    const name = drivers.filter(driver => driver.name == string)
+    return name
+}
